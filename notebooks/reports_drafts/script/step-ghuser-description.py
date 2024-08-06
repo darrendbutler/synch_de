fa5098280b59b65830d68876a8ed0a34ee4de99d
Darren Butler <42709036+darrendbutler@users.noreply.github.com>
2024-08-06 00:46:36 -0400

    run nbautoexport

---
+++ notebooks/reports_drafts/script/step-ghuser-description.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Template Notebook
+# This notebook is a template. Please make a copy and following the conventions for naming and structure.
+
+"""This is a docstring."""
+
