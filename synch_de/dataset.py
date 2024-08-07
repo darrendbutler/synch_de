35809f4cb64f7bc5e29c52f8057bb63e7ceceb85
Darren Butler <42709036+darrendbutler@users.noreply.github.com>
2024-08-07 13:14:52 -0400

    add read course table function and dummy function for testing

--- synch_de/dataset.py
+++ synch_de/dataset.py
@@ -5,15 +5,43 @@ from pathlib import Path
 import typer
 from loguru import logger
 from tqdm import tqdm
+import pandas as pd
+import dask as dd
 
 from synch_de.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 
 def greet():
     """Greet the user. This function is used for as an example during onboarding."""
-    logger.info("Hello, welcome to the dataset processing script!")
+    logger.info(
+        "Hello, welcome to the dataset processing script!"
+    )
+    return "Hi!"
 
 
+def read_course_table() -> pd.DataFrame:
+    """Read the course table."""
+    df = pd.read_csv(
+        RAW_DATA_DIR / "course.csv",
+        parse_dates=["start", "stop", "created", "updated"],
+        dtype={
+            "title": "category",
+            "path": "category",
+            "status": "category",
+        },
+    )
+    return df
+
+
+def return_int(value: int) -> int:
+    """Returns the input value.
+    This function is used for as an example during onboarding."""
+    return value
+
+
+read_course_table().info()
+
+############## Refactor main functions below ##############
 app = typer.Typer()
 
 
@@ -24,13 +52,7 @@ def main(
     output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
     # ----------------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
+    pass
 
 
 if __name__ == "__main__":
