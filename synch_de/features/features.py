e973476e6054eac86dea8f9f809536307849279a
Darren Butler <42709036+darrendbutler@users.noreply.github.com>
2024-09-15 09:29:16 -0400

    ref: calculate exam features with include_groups flag

--- synch_de/features/features.py
+++ synch_de/features/features.py
@@ -5,6 +5,10 @@ import typer
 from loguru import logger
 
 from synch_de.config import PROCESSED_DATA_DIR, INTERIM_DATA_DIR
+from synch_de.features.exam import (
+    calculate_exam_features,
+    extract_exam_responses,
+)
 from synch_de.features.prior_insutrction import (
     calculate_prior_instruction_features,
     extract_prior_instruction_data,
@@ -36,34 +40,46 @@ def main(
 ):
     logger.info("Generating features from dataset...")
 
+    # Read preprocessed responses
     input_path = INTERIM_DATA_DIR / "processed_responses.pkl"
-    df = pd.read_pickle(input_path)
+    preprocessed_responses = pd.read_pickle(input_path)
 
+    # Initialize the features dataframe
     features = pd.DataFrame()
 
-    prior_instruction_data = extract_prior_instruction_data(df)
+    # Extract responses to exam questions and calculate exam features
+    exam_responses = extract_exam_responses(preprocessed_responses)
+    create_data_profile(exam_responses, "exam_responses.pkl")
+    exam_responses.to_pickle(INTERIM_DATA_DIR / "exam_responses.pkl")
+
+    # Calculate exam features
+    exam_features = exam_responses.groupby(
+        by="user_id",
+        observed=True,
+    ).apply(calculate_exam_features, include_groups=False,)
+
+    # Create Prior Instruction Features
+    prior_instruction_data = extract_prior_instruction_data(
+        preprocessed_responses
+    )
     prior_insutrction_features = calculate_prior_instruction_features(
         prior_instruction_data
     )
-
-    # Get a subset of the data for user 42930 for testing purposes
-    responses_from_one_user = prior_instruction_data[
-        prior_instruction_data["user_id"] == "42930"
-    ].copy()
-
-    # Concatenate the features to the features dataframe
-    features = pd.concat([features, prior_insutrction_features], axis=1)
-
-    prior_instruction_data["user_id"].value_counts()
-
-    logger.success("Features generation complete.")
-
     # Create a data profile for the prior_instruction data
     create_data_profile(prior_instruction_data, "prior_insutrction_data.pkl")
     # Save the prior_instruction_data to INTERIM_DATA_DIR
     prior_instruction_data.to_pickle(
         INTERIM_DATA_DIR / "prior_instruction_data.pkl"
     )
+
+    # Concatenate the features to the features dataframe
+    features = pd.concat([features, prior_insutrction_features], axis=1)
+
+    # note: in cleaning, drop user_id 44758 because they
+    # took both exams, may be staff
+    # Consider how you'd like to deal with scaling and outliers
+
+    logger.success("Features generation complete.")
 
     # -----------------------------------------
 
